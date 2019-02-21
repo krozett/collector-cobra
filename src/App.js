b919152b84ea4ce4f443d830a0340a7d3d34e835
Keith Rozett <krozett@gmail.com>
2019-02-20 22:13:15 -0800

    Version updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import Button from '@material-ui/core/Button'
+import DeleteIcon from '@material-ui/icons/Delete'
 
 class App extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class App extends React.Component {
         <CssBaseline />
         <p>Cobras!</p>
         <Button>clicko</Button>
+        <DeleteIcon />
       </>
     )
   }
