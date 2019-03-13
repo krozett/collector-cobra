21e458f6fb07048d0d6e076451ce3ccfef8f3854
Keith Rozett <krozett@gmail.com>
2019-03-12 23:14:51 -0700

    Config

---
+++ src/config.sample.js
@@ -0,0 +1,10 @@
+const config = {
+  firebase: {
+    projectId: 'collector-cobra',
+    apiKey: 'its_a_secret_to_everybody',
+    authDomain: 'collector-cobra.firebaseapp.com',
+    databaseURL: 'https://collector-cobra.firebaseio.com'
+  }
+}
+
+export default config
