d4302271e2de26a906ef58eebc05d39bf58f3639
Keith Rozett <krozett@gmail.com>
2020-05-12 23:48:48 -0700

    Remove firebaseui

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import firebase from 'firebase'
-import { auth } from 'firebaseui'
 
 import { authInit } from 'store/user'
 
@@ -13,33 +12,26 @@ class Auth extends React.Component {
   }
 
   render() {
-    let loading = null
-
     if (!this.props.user || !this.props.user.hasOwnProperty('isAnonymous')) {
-      loading = (
+      return (
         <div>loading...</div>
       )
     }
 
     if (this.props.user && this.props.user.isAnonymous) {
-      const ui = new auth.AuthUI(firebase.auth())
-
-      ui.start('#firebaseui-auth-container', {
-        signInOptions: [
-          firebase.auth.GoogleAuthProvider.PROVIDER_ID
-        ],
-        callbacks: {
-          signInSuccessWithAuthResult: (authResult, redirectUrl) => false
-        }
-      })
+      return (
+        <button onClick={this.signIn}>
+          Log In with Google
+        </button>
+      )
     }
 
-    return (
-      <>
-        <div id="firebaseui-auth-container"></div>
-        {loading}
-      </>
-    )
+    return null
+  }
+
+  signIn = () => {
+    const provider = new firebase.auth.GoogleAuthProvider()
+    firebase.auth().signInWithRedirect(provider)
   }
 }
 
