9abc231c55f1ed87476da901ab516c7996b07675
Keith Rozett <krozett@gmail.com>
2025-10-09 13:12:09 -0700

    React apparently cares how env vars are named

--- src/index.js
+++ src/index.js
@@ -11,12 +11,12 @@ import 'index.css'
 import 'react-md/dist/react-md.green-deep_orange.min.css'
 
 initializeApp({
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 })
 
 // Allow functions URL base to be overridden
